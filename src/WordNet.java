124ff13a9f99f6f6829b3c6ba06ce9560053957c
Karl DeBisschop <karl.debisschop@fen.com>
2017-10-30 07:21:22 -0400

    Initial commit

---
+++ src/WordNet.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Karl DeBisschop on 2017-10-29.
+ */
+
+import edu.princeton.cs.algs4.In;
+
+public class WordNet {
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+
+    }
+}
